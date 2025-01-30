838b2535cc872ff1984ed2eb09c3c70dee5d2aba
cosmicpotato137 <imacsunde@gmail.com>
2025-01-29 23:26:34 -0500

    bug fix

--- backend/printrun/views.py
+++ backend/printrun/views.py
@@ -4,10 +4,7 @@ import subprocess
 import traceback
 import os
 
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 
 def printrun_view(request):
@@ -16,23 +13,16 @@ def printrun_view(request):
         command = request.POST.get("command")
         if command:
             try:
-                print("here")
-                if os.name == 'nt':  # Check if the OS is Windows
-                    process = subprocess.Popen(
-                        ["pronsole"],
-                        stdin=subprocess.PIPE,
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                        text=True
-                    )
-                else:  # Assume the OS is Linux/Unix
-                    process = subprocess.Popen(
-                        ["sudo", "pronsole"],
-                        stdin=subprocess.PIPE,
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                        text=True
-                    )
+                env = os.environ.copy()
+                env['PRONSOLE_CONFIG_DIR'] = '/path/to/accessible/directory'
+                process = subprocess.Popen(
+                    ["pronsole"],
+                    stdin=subprocess.PIPE,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    text=True,
+                    env=env
+                )
                 stdout, stderr = process.communicate(input=command)
                 response = f"<pre>{stdout}</pre><pre>{stderr}</pre>"
             except Exception as e:
