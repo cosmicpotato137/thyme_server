26c89a9fb4720cf716666e0941ca12b52e6180c3
cosmicpotato137 <imacsunde@gmail.com>
2025-02-02 13:52:27 -0500

    updated settings

--- backend/thyme_server/settings.py
+++ backend/thyme_server/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "thyme", "paprika.ddns.net"]
 
 # SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
 
